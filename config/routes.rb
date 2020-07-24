5e60329cf44257a47bbaf02f5ba49d8e3d34b9c8
Paul Omondi <paulo.techie@gmail.com>
2020-07-25 01:55:56 +0300

    remove unauthorized edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   resources :posts
-  devise_for :users, controllers:{ registrations: 'registrations'}
+  devise_for :users, controllers: { registrations: 'registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "posts#index"
+  root 'posts#index'
 end
