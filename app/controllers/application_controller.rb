8e7968f310829348ab521f4db6e116a82308f398
Paul Omondi <paulo.techie@gmail.com>
2020-07-27 13:04:59 -0600

    indent private

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   helper_method :post_owner
 
-    private
+      private
 
   def post_owner
-    return if (current_user.id == @post.user_id)
+    return if current_user.id == @post.user_id
+    
     flash[:notice] = 'Access denied as you are not the owner of that post'
     redirect_to posts_url
   end
