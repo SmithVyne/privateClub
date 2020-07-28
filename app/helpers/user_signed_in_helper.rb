4cf356f43d68c34f7ba39c2ad83b83bec18baaf3
SmithVyne <smithnkereuwem2@gmail.com>
2020-07-29 01:19:46 +0300

    fix lint errors

--- app/helpers/user_signed_in_helper.rb
+++ app/helpers/user_signed_in_helper.rb
@@ -1,20 +1,15 @@
 module UserSignedInHelper
-
   def show_post_author(post)
-    if user_signed_in?
-        post.user.name
-    end
+    post.user.name if user_signed_in?
   end
-  
+
   def user_can_edit(post)
-    if user_signed_in? && (current_user.id == post.user_id)
-        link_to 'Edit', edit_post_path(post)
-    end
+    link_to 'Edit', edit_post_path(post) if user_signed_in? && (current_user.id == post.user_id)
   end
 
   def user_can_destroy(post)
     if user_signed_in? && (current_user.id == post.user_id)
-        link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }
+      link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }
     end
   end
 end
