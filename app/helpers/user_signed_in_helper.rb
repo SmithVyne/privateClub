65b43d3b46b4de284144fed8d9eedb7202bb7292
SmithVyne <smithnkereuwem2@gmail.com>
2020-07-29 01:42:29 +0300

    fixed line-length error

--- app/helpers/user_signed_in_helper.rb
+++ app/helpers/user_signed_in_helper.rb
@@ -4,10 +4,14 @@ module UserSignedInHelper
   end
 
   def user_can_edit(post)
-    link_to 'Edit', edit_post_path(post) if user_signed_in? && (current_user.id == post.user_id)
+    link_to 'Edit', edit_post_path(post) if signed_in_current_user(post)
   end
 
   def user_can_destroy(post)
-      link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' } if user_signed_in? && (current_user.id == post.user_id)
+      link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' } if signed_in_current_user(post)
+  end
+
+  def signed_in_current_user(post)
+    return true if user_signed_in? && (current_user.id == post.user_id)
   end
 end
